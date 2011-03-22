260030c372dd013d816aba67c26374dd4f1524ed
João Vitor Lacerda Guimarães <joao.vitor@gmail.com>
2011-03-22 10:56:37 -0300

    updated java config

--- profile.d/java.sh
+++ profile.d/java.sh
@@ -9,22 +9,25 @@ JAVA_SERVERS_DIR=$HOME/programs/java/servers
 export   JAVA_HOME=$(/usr/libexec/java_home)
 export    ANT_HOME=$JAVA_PROG_DIR/ant
 export    MVN_HOME=$JAVA_PROG_DIR/maven
+export AWS_IAM_HOME=$JAVA_PROG_DIR/IAMCli
+export GRADLE_HOME=$JAVA_PROG_DIR/gradle
 
-#export    ROO_HOME=$JAVA_PROG_DIR/spring-roo
-#export  SCALA_HOME=$JAVA_LANG_DIR/scala
-#export GROOVY_HOME=$JAVA_LANG_DIR/groovy
-#export GRAILS_HOME=$JAVA_PROG_DIR/grails
-#export  JRUBY_HOME=$JAVA_LANG_DIR/jruby
+export    ROO_HOME=$JAVA_PROG_DIR/spring-roo
+export  SCALA_HOME=$JAVA_LANG_DIR/scala
+export GROOVY_HOME=$JAVA_LANG_DIR/groovy
+export GRAILS_HOME=$JAVA_PROG_DIR/grails
 #export TOMCAT_HOME=$JAVA_SERVERS_DIR/tomcat
 
 export PATH=$JAVA_HOME/bin:$PATH
 export PATH=$ANT_HOME/bin:$PATH
 export PATH=$MVN_HOME/bin:$PATH
-#export PATH=$SCALA_HOME/bin:$PATH
-#export PATH=$GROOVY_HOME/bin:$PATH
-#export PATH=$JRUBY_HOME/bin:$PATH
-#export PATH=$GRAILS_HOME/bin:$PATH
-#export PATH=$PATH:$ROO_HOME/bin:$PATH
+export PATH=$AWS_IAM_HOME/bin:$PATH
+export PATH=$GRADLE_HOME/bin:$PATH
+
+export PATH=$SCALA_HOME/bin:$PATH
+export PATH=$GROOVY_HOME/bin:$PATH
+export PATH=$GRAILS_HOME/bin:$PATH
+export PATH=$PATH:$ROO_HOME/bin:$PATH
 
 export ANDROID_HOME=$JAVA_SDK_DIR/android
 export PATH=$ANDROID_HOME/tools:$PATH
