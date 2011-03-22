f807878a15b49910a46fadf2244518032b158dbb
João Vitor Lacerda Guimarães <joao.vitor@gmail.com>
2011-03-22 10:55:29 -0300

    git config

--- profile.d/git.sh
+++ profile.d/git.sh
@@ -1,3 +1,21 @@
+# echo ".DS_Store" >> ~/.gitignore
+# git config --global core.excludesfile ~/.gitignore
+
+# git config --global user.name "Your Name Comes Here"
+# git config --global user.email you@yourdomain.example.com
+# git config --global core.editor vim
+# git config --global push.default current
+
+# git config --global alias.unstage 'reset HEAD'
+# git config --global alias.st status
+# git config --global alias.ci commit
+# git config --global alias.co checkout
+# git config --global alias.br branch
+# git config --global color.branch auto
+# git config --global color.diff auto
+# git config --global color.interactive auto
+# git config --global color.status auto
+
 alias gba='git branch -a'
 alias gcm='git checkout master'
 alias glpp='git log --pretty=oneline'
