5088cafcbb7ac23f81d5e4d6d1f6d6193e96fdfc
João Vitor Lacerda Guimarães <joao.vitor@gmail.com>
2012-02-09 02:15:30 -0200

    added maxdepth to find do link only direct files or directories inside dotfiles dir

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 dotfiles_basedir=$1
 
-for i in `find ${dotfiles_basedir}/dotfiles/`; do
+for i in `find ${dotfiles_basedir}/dotfiles/ -maxdepth 1`; do
   echo $i;
   original_file=`basename $i`
   ln -snfv $i .${original_file}
